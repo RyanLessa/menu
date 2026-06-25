f18837a94e25e0e819817330a695d51eefa258ea
RyanLessa <ryan.lessa@icloud.com>
2026-06-25 16:33:26 -0300

    improving annotations type

--- src/routes.ts
+++ src/routes.ts
@@ -5,39 +5,63 @@ import type { Request, Response } from 'express'
 
 const route = Router()
 
+interface MenuItemInterface {
+  route: string;
+  filename: string;
+}
+
+class MenuItem implements MenuItemInterface {
+  route: string;
+  filename: string;
+
+  constructor(route: string, filename: string) {
+    this.route = route;
+    this.filename = filename;
+  }
+
+  getPath(base: string) {
+    return path.join(base, this.filename);
+  }
+}
+
+const home = new MenuItem("/", "home.html")
+const navegador = new MenuItem("/navegador", "navegador.html")
+const contatos = new MenuItem("/contatos", "contatos.html")
+const musica = new MenuItem("/musica", "musica.html")
+
 const TEMPLATEPATH = path.join(path.resolve("."), "src", "templates")
 
-route.get('/', (req: Request, res: Response) => {
+route.get(home.route, (req: Request, res: Response) => {
   const templatePath = path.join(
     TEMPLATEPATH,
-    "home.html"
+    home.filename
   )
 
   res.sendFile(templatePath)
 })
 
-route.get('/navegador', (req: Request, res: Response) => {
+route.get(navegador.route, (req: Request, res: Response) => {
   const templatePath = path.join(
     TEMPLATEPATH,
-    "navegador.html"
+    navegador.filename
   )
 
   res.sendFile(templatePath)
 })
 
-route.get('/contatos', (req: Request, res: Response) => {
+route.get(contatos.route, (req: Request, res: Response) => {
   const templatePath = path.join(
     TEMPLATEPATH,
-    "contatos.html"
+    contatos.filename
   )
 
   res.sendFile(templatePath)
 })
 
-route.get('/musica', (req: Request, res: Response) => {
+route.get(musica.route, (req: Request, res: Response) => {
   const templatePath = path.join(
     TEMPLATEPATH,
-    "musica.html"
+    musica.filename
   )
 
   res.sendFile(templatePath)
